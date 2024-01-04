6fbd7eb979838189ed1dc9a606ee1fa6f82eab47
kushal1997 <kushalrao103@gmail.com>
2024-01-04 16:58:19 +0530

    first commit

---
+++ first one/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server= http.createServer((req,res)=>{
+    console.log(req.url)
+    res.write("Welcome to node js server")
+    if(req.url=="/jobs") {
+       return res.end("You are on job Portal")
+    
+    }
+    
+})
+
+server.listen(3100,()=>{
+    console.log("Server is running at port 3100")
+});
